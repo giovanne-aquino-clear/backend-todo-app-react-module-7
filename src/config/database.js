43b672535f2ae329610aba99a7bd5bca318ba3ca
giovanne <giovanne.aquino.clear@gmail.com>
2023-09-21 10:12:50 -0300

    config db

--- src/config/database.js
+++ src/config/database.js
@@ -1,2 +1,6 @@
 const mongoose = require('mongoose')
-mongoose.Promisse  
+mongoose.Promise = global.Promise
+module.exports =  mongoose.connect("mongodb://localhost:27017/todo", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
