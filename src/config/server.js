316f4a465cfc4dcfec9f1b90fd154cef258ce605
giovanne <giovanne.aquino.clear@gmail.com>
2023-09-21 10:13:58 -0300

    update: change server

--- src/config/server.js
+++ src/config/server.js
@@ -3,10 +3,16 @@ const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+const allowCors = require('./cors')
+const routes = require('./routes')
 
-server.use(bodyParser.urlencoded({ extended: true}))
+server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
+server.use(routes)
 
-server.listen(port,function(){
-    console.log('rodo fi')
+server.listen(port, function() {
+    console.log(`BACKEND is running on port ${port}.`)
 })
+
+module.exports = server
